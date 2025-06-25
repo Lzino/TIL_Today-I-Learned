00d3541d905daa72b1a8934c6dd165be0a0e3329
Woodam Lim <woodam.lim@gmail.com>
2025-06-25 17:12:42 +0900

    Update 250625_Image_similarity.py

--- Coding_test/250625_Image_similarity.py
+++ Coding_test/250625_Image_similarity.py
@@ -6,9 +6,11 @@ def lcs_similarity(x: str, y: str) -> float:
     # 표 채우기
     for i in range(1, n + 1):          # X의 i번째 문자까지
         for j in range(1, n + 1):      # Y의 j번째 문자까지
-            if x[i - 1] == y[j - 1]:   # 글자 일치(↖)
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:                      # 글자 불일치(↑, ← 중 큰 값)
+            if x[i - 1] == y[j - 1]:   # 글자가 일치하면
+                # DP에 반영. X,Y는 0부터 시작하지만 DP는 숫자 그대로 사용
+                dp[i][j] = dp[i - 1][j - 1] + 1  
+            else:
+                # 불일치하면 X 이전 단계나 Y 이전 단계 중 큰 걸 반영
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
     # LCS 길이 → 유사도(%) 계산
