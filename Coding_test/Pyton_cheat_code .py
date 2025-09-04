5bbccf64b24cd2d3f9fd69217fad62c679b92da2
Woodam Lim <woodam.lim@gmail.com>
2025-09-04 13:12:21 +0900

    Update Pyton_cheat_code .py

--- Coding_test/Pyton_cheat_code .py
+++ Coding_test/Pyton_cheat_code .py
@@ -9,6 +9,27 @@ for letter in word :
 
 print(word_count)
 
+# 펠린드롬(회문)
+## 방법 1 : 1대1 대응
+for i in range(len(word)):
+    if word[i] == word[len(word)-1-i]:
+        print(1)
+    else :
+        print(0)
+
+## 방법 2 : reversed(iterabla) 사용. reverse()는 None값 배출 (리스트만 바꿈) 
+for i in range(len(word)):
+    if word[i] == list(reversed(word)):
+        print(1)
+
+## 방법 3 : 문자열 슬라이싱
+## 문자열 :  [start : end : step]
+for i in range(len(word)):
+    if word == word[::-1]: # 거꾸로 해보기
+        print(1)
+
+
+
 # 정규 표현식
 import re 
 temp = re.split('[^0-9]', list)
@@ -18,6 +39,11 @@ temp = re.split('[^0-9]', list)
 # 리스트를 문자열로 합치기
 ## ['a', 'b', 'c'] →  'abc'
 temp = ''.join(list)
+
+# 에라토스테네스의 체 (소수 구하기)
+
+# 누적합 만들기
+
 
 # SQL 주요 함수 
 ## 추출
